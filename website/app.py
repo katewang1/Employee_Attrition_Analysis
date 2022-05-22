f0660735a4e1b27979b3134c6984cac704cce0d9
Show Wang <show.wang94@gmail.com>
2022-05-21 23:55:18 -0700

    add pickle serialization and logistic regression route for flask

--- website/app.py
+++ website/app.py
@@ -1,14 +1,14 @@
 # import libraries
 # add one for connection to aws or database
 from flask import Flask, render_template, request
-# import pickle
+import pickle
 import numpy as np
 
 # boilerplate; create Flask app instance
 app = Flask(__name__)
 
 # read from external model.pkl file
-# model=pickle.load(open("model.pkl","rb"))
+model=pickle.load(open("model.pkl","rb"))
 
 # create root route
 @app.route("/")
@@ -18,10 +18,14 @@ def root():
 # create route for logistic regression ml model prediction
 @app.route("/attrition/logistic_regression", methods=["POST", "GET"])
 def lr():
-    # features=[request.form.values()]
-    # features_array=[np.array(features)]
-    # return 
+    features=[request.form.values()]
+    features_array=[np.array(features)]
+    prediction = model.predict_prob(features_array)
+    output = "{0:.{1}f}".format(prediction[0][1], 2)
     
+    return render_template("index.html", pred = "Based on the Logistic Regression model, probability of your employee leaving the company is {}".format(output))
+    
+    # ANDREW GAVE ME THE NEXT 9 LINES; WHOEVER TOUCHES IT DIES >:) 
     if request.method == "POST":
         variable = request.form.get("test")
         return f"<h1> {variable} </h1>"
